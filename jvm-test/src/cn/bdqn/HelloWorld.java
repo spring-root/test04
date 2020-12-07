7125882572528d08bcde5369f956c29090282e7f
zhangsan <zhangsan@qq.com>
2020-12-07 11:52:04 +0800

    小王添加了HelloWorld实体类

---
+++ jvm-test/src/cn/bdqn/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.bdqn;
+
+/**
+ * @author LiSheng
+ * @create 2020-12-07 11:51
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("111111111111111111111111111");
+    }
+
+}
