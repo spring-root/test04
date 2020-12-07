3801863412202eefa10307e9448898776df59feb
zhangsan <zhangsan@qq.com>
2020-12-07 12:02:08 +0800

    SVN第er次提交

--- jvm-test/src/cn/bdqn/HelloWorld.java
+++ jvm-test/src/cn/bdqn/HelloWorld.java
@@ -8,17 +8,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("111111111111111111111111111");
         System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("111111111111111111111111111");
-        System.out.println("2222222222222222222222222222");
+
     }
 
 }
